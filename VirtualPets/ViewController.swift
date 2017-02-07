c1ec870723c33b8dca0646496082a6f1fa019779
DQL433 <TIM@timdemacbook-pro.local>
2017-02-06 21:06:14 -0600

    Initial Commit

---
+++ VirtualPets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualPets
+//
+//  Created by TIM on 2/6/17.
+//  Copyright © 2017 TIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
