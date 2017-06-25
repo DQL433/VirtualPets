d25223d768e2c39158439c99658d2d8666978782
DQL433 <DQL433@gmail.com>
2017-06-25 01:11:39 -0400

    Add files via upload

---
+++ VirtualPets/classPet.swift
@@ -0,0 +1,25 @@
+//
+//  classPet.swift
+//  VirtualPets
+//
+//  Created by TIM on 2/8/17.
+//  Copyright © 2017 TIM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Pet {
+    var h : Int
+    var f : Int
+    var fn: Int
+    var color:UIColor
+    init(color:UIColor){
+    self.h = 0
+    self.f = 0
+    self.fn = 0
+    self.color = color
+    }
+
+}
